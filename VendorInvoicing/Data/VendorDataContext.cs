eefd45946698b095683afbf88c86e04af85dd9bb
tehtko <moderndsnsam@gmail.com>
2022-11-19 16:20:03 -0500

    Add DbSets

---
+++ VendorInvoicing/Data/VendorDataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.IO;
+using VendorInvoicing.Entities;
+
+namespace VendorInvoicing.Data
+{
+    public class VendorDataContext : DbContext
+    {
+        public VendorDataContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceLineItem> InvoiceItems { get; set; }
+        public DbSet<PaymentTerms> PaymentTerms { get; set; }
+    }
+}
