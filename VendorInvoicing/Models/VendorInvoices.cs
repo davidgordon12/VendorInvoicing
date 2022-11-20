77c22d7b1c2b29e6e2fc915760516be79dc4c7e4
tehtko <moderndsnsam@gmail.com>
2022-11-20 16:58:05 -0500

    Create VendorInvoice view model

---
+++ VendorInvoicing/Models/VendorInvoices.cs
@@ -0,0 +1,10 @@
+﻿using VendorInvoicing.Entities;
+
+namespace VendorInvoicing.Models
+{
+    public class VendorInvoices
+    {
+        public Vendor? Vendor { get; set; }
+        public List<InvoiceLineItem>? InvoiceLineItems { get; set; }
+    }
+}
