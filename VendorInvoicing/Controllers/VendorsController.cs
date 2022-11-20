96515bc1bab2c657eed6a25db2824d0e086dba99
tehtko <moderndsnsam@gmail.com>
2022-11-20 16:57:52 -0500

    Move data access to VendorService

--- VendorInvoicing/Controllers/VendorsController.cs
+++ VendorInvoicing/Controllers/VendorsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using VendorInvoicing.Data;
 using VendorInvoicing.Entities;
+using VendorInvoicing.Models;
 using VendorInvoicing.Services;
 
 namespace VendorInvoicing.Controllers
@@ -20,7 +21,6 @@ namespace VendorInvoicing.Controllers
             _vendorService = vendorService;
         }
 
-        // GET: Vendors
         public IActionResult Index(char? id)
         {
             if (id is null)
@@ -29,137 +29,72 @@ namespace VendorInvoicing.Controllers
             return View(_vendorService.GetVendorsByRange(id));
         }
 
-        //// GET: Vendors/Details/5
-        //public IActionResult Details(int? id)
-        //{
-        //    if (id == null || _context.Vendors == null)
-        //    {
-        //        return NotFound();
-        //    }
+        public IActionResult Invoices(int? id, int? invoiceId)
+        {
+            if (id is null)
+                return View("Index", _vendorService.GetVendors());
 
-        //    var vendor = _context.Vendors
-        //        .FirstOrDefault(m => m.VendorId == id);
-        //    if (vendor == null)
-        //    {
-        //        return NotFound();
-        //    }
+            if (invoiceId is null)
+                return View(new VendorInvoices
+                {
+                    Vendor = _vendorService.GetVendor(id),
+                    InvoiceLineItems = Enumerable.Empty<InvoiceLineItem>().ToList(),
+                });
+                
+            VendorInvoices vendorInvoices = _vendorService.GetVendorInvoices(id, invoiceId);
+            return View(vendorInvoices);
 
-        //    return View(vendor);
-        //}
+            
+        }
 
-        //// GET: Vendors/Create
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-        //// POST: Vendors/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Create(Vendor vendor)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(vendor);
-        //        _context.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(vendor);
-        //}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Vendor vendor)
+        {
+            if (ModelState.IsValid)
+            {
+                if(_vendorService.AddVendor(vendor)) // returns true if the vendor was added successfully
+                    return RedirectToAction("Index");
+            }
+            return View(vendor);
+        }
 
-        //// GET: Vendors/Edit/5
-        //public IActionResult Edit(int? id)
-        //{
-        //    if (id == null || _context.Vendors == null)
-        //    {
-        //        return NotFound();
-        //    }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        //    var vendor = _context.Vendors.Find(id);
-        //    if (vendor == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(vendor);
-        //}
+            Vendor vendor = _vendorService.GetVendor(id);
+            return View(vendor);
+        }
 
-        //// POST: Vendors/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Edit(int id, Vendor vendor)
-        //{
-        //    if (id != vendor.VendorId)
-        //    {
-        //        return NotFound();
-        //    }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Vendor vendor)
+        {
+            if (ModelState.IsValid)
+            {
+                if(_vendorService.UpdateVendorInformation(vendor))
+                    return RedirectToAction("Index");
+            }
+            return View(vendor);
+        }
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(vendor);
-        //            _context.SaveChanges();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!VendorExists(vendor.VendorId))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(vendor);
-        //}
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+                return View("Index");
 
-        //// GET: Vendors/Delete/5
-        //public IActionResult Delete(int? id)
-        //{
-        //    if (id == null || _context.Vendors == null)
-        //    {
-        //        return NotFound();
-        //    }
+            _vendorService.RemoveVendor(id);
+            return View();
+        }
 
-        //    var vendor = _context.Vendors
-        //        .FirstOrDefault(m => m.VendorId == id);
-        //    if (vendor == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(vendor);
-        //}
-
-        //// POST: Vendors/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult DeleteConfirmed(int id)
-        //{
-        //    if (_context.Vendors == null)
-        //    {
-        //        return Problem("Entity set 'VendorDataContext.Vendors'  is null.");
-        //    }
-        //    var vendor = _context.Vendors.Find(id);
-        //    if (vendor != null)
-        //    {
-        //        _context.Vendors.Remove(vendor);
-        //    }
-
-        //    _context.SaveChanges();
-        //    return RedirectToAction(nameof(Index));
-        //}
-
-        //private bool VendorExists(int id)
-        //{
-        //    return _context.Vendors.Any(e => e.VendorId == id);
-        //}
     }
 }
