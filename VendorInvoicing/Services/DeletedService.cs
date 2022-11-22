d9a1f1e66c07491288146c22ae70e0ee23c3648e
tehtko <moderndsnsam@gmail.com>
2022-11-21 21:56:14 -0500

    Create a Singleton services that contains the last deleted Vendor's id

---
+++ VendorInvoicing/Services/DeletedService.cs
@@ -0,0 +1,20 @@
+﻿namespace VendorInvoicing.Services
+{
+    public class DeletedService
+    {
+        private static Dictionary<string, int> deleted = new Dictionary<string, int>();
+
+        public static void AddLastDeleted(int id)
+        {
+            deleted["lastDeleted"] = id;
+        }
+
+        public static int GetLastDeleted()
+        {
+            if (deleted.ContainsKey("lastDeleted"))
+                return deleted["lastDeleted"];
+            else
+                return 0;
+        }
+    }
+}
