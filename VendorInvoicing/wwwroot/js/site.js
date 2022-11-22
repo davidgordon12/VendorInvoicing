12ac6d1ba8598fe1f77451f87310a15360f24154
tehtko <moderndsnsam@gmail.com>
2022-11-21 21:57:10 -0500

    Implement alert close button

--- VendorInvoicing/wwwroot/js/site.js
+++ VendorInvoicing/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('#alert-close-btn').click(() => {
+    $('#alert').fadeOut();
+})
